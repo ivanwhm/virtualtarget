ecbd5f98c841e41e9058f45afa05ba13a85d9fc0
Ivan Wilhelm <ivan.whm@me.com>
2012-11-05 15:53:53 -0200

    Cria exemplo para criação de envios através de webservice.

---
+++ exemplos/envios/exemplo_cria_novo_envio.php
@@ -0,0 +1,38 @@
+<?php
+
+  //Ajusta a codificação e o tipo do conteúdo
+  header('Content-type: text/txt; charset=utf-8');
+
+  //Importa as classes
+  require '../../classes/VirtualTarget.php';
+  require '../../classes/envios/VirtualTargetCriaNovoEnvio.php';
+
+  //Importa
+  try
+  {
+    $criar = new VirtualTargetCriaNovoEnvio('', '');
+    $criar->setRemetenteNome('Teste');
+    $criar->setRemetenteEmail('teste@teste.com.br');
+    $criar->setRemetenteReply('teste@teste.com.br');
+    $criar->setAssunto('Teste de envio');
+    $criar->setMensagem('Teste');
+    $criar->setDataHoraProgramada(new DateTime());
+    $criar->setNotificacaoEmail('teste@teste.com.br');
+    $criar->addFiltro(array(array(
+        'id' => 1,
+        'campo' => '',
+        'valores' => array()
+      )), array(
+      'id' => 141,
+      'campo' => '',
+      'valores' => array()
+    ));
+    $retorno = $criar->processa();
+    echo '<pre>';
+    var_dump($retorno);
+    echo '</pre>';
+  } catch (Exception $exc)
+  {
+    echo $exc->getMessage();
+  }
+?>
